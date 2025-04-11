533e9edff96ad68274fda4b1831d7e7ae4a6b31e
bestwall2 <git-lfs.github.com>
2025-04-11 21:53:59 +0000

    test 1

--- components/ListItems.tsx
+++ components/ListItems.tsx
@@ -93,7 +93,7 @@ const ListItems = ({ geners, apiPath }: ListItemsProps) => {
                 >
                 <Link href={animeUrl} passHref>
                     <DiscoverCard
-                    cardbadge={(anime.averageScore / 10).toString()}
+                    cardbadge={anime.averageScore ? `${anime.averageScore / 10}` : "N/A"}
                     title={anime.title.english || anime.title.romaji || "Unknown Title"}
                     info={`${anime.format} • ${anime.startDate?.year || "Unknown Year"} • ${anime.episodes || "N/A"} Episodes`}
                     img={anime.coverImage.extraLarge}
