cf1e3ed909cb25085c39f3e1708b28357fe39528
bestwall2 <git-lfs.github.com>
2025-04-15 18:54:25 +0000

    Add Relation ListvTo Info Page

--- components/CardsComp/EpisodesCard.tsx
+++ components/CardsComp/EpisodesCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const EpisodeCard = ({ title, description, image, number, imgbup }) => {
   return (
-    <div className="bg-zinc-800 rounded-xl shadow-lg flex items-center space-x-2 p-2">
+    <div className="bg-zinc-800 rounded-xl shadow-lg flex items-center space-x-1 p-2">
       <div className="relative w-[120px] h-full rounded-lg overflow-hidden flex-shrink-0">
         <Image
             src={image.includes('https://') ? image : imgbup}
@@ -15,7 +15,7 @@ const EpisodeCard = ({ title, description, image, number, imgbup }) => {
 
       <div className="flex flex-col overflow-hidden">
         <h3 className="text-sm font-semibold text-white truncate">{`${number}. ${title}`}</h3>
-        <p className="text-sm text-gray-300 line-clamp-2">{description.includes('$undefined') ? "" : description}</p>
+        <p className="text-sm text-gray-300 line-clamp-2">{description.includes('$undefined') ? imgbup : description}</p>
       </div>
     </div>
   );
--- components/Episodes.tsx
+++ components/Episodes.tsx
@@ -17,10 +17,10 @@ interface EpisodesProps {
 
 const Episodes: React.FC<EpisodesProps> = ({ episodes }) => {
   return (
-    <div className="mt-4 h-[400px]">
+    <div className="mt-4">
       <div className="flex text-gray-200 items-center mb-4 space-x-2">
         <span className="w-1.5 rounded-full h-6 bg-indigo-500"></span>
-        <p className="text-md font-semibold">Episodes</p>
+        <p className="text-md font-semibold">Anime Episodes</p>
       </div>
 
       <div className="flex flex-col gap-4">
