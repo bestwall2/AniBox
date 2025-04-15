9b67ebcd7029d5ef079e53920d26d1088c0840a8
bestwall2 <git-lfs.github.com>
2025-04-15 18:43:36 +0000

    Add Relation ListvTo Info Page

--- components/CardsComp/EpisodesCard.tsx
+++ components/CardsComp/EpisodesCard.tsx
@@ -5,7 +5,7 @@ const EpisodeCard = ({ title, description, image, number, imgbup }) => {
     <div className="bg-zinc-800 rounded-xl shadow-lg flex items-center space-x-4 p-2">
       <div className="relative w-[100px] h-[80px] rounded-lg overflow-hidden flex-shrink-0">
         <Image
-            src={image || imgbup}
+            src={image.includes('https://') ? image : imgbup}
             alt={title}
             width={160}
             height={90}
@@ -15,7 +15,7 @@ const EpisodeCard = ({ title, description, image, number, imgbup }) => {
 
       <div className="flex flex-col overflow-hidden">
         <h3 className="text-sm font-semibold text-white truncate">{`${number}. ${title}`}</h3>
-        <p className="text-sm text-gray-300 line-clamp-2">{description || ""}</p>
+        <p className="text-sm text-gray-300 line-clamp-2">{description.includes('$undefined') ? "" : description}</p>
       </div>
     </div>
   );
