0638cc5a51d0858b7c301285d12091d6595ab5be
bestwall2 <git-lfs.github.com>
2025-04-16 14:32:05 +0000

    Add Relation ListvTo Info Page

--- components/NProgress.tsx
+++ components/NProgress.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import NProgress from 'nprogress';
-
+import 'nprogress/nprogress.css';
 
 NProgress.configure({ showSpinner: false });
 
