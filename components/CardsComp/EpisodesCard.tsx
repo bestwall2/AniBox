0c00aecfa7ecfa8e7c2b811674227c87d30be69a
bestwall2 <git-lfs.github.com>
2025-04-16 02:12:34 +0000

    Add Relation ListvTo Info Page

--- components/CardsComp/EpisodesCard.tsx
+++ components/CardsComp/EpisodesCard.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const EpisodeCard = ({ title, description, image, number, imgbup }) => {
   return (
-    <div className="bg-zinc-800 transition-all duration-300 ease-out hover:scale-[0.97] rounded-xl shadow-lg flex items-center space-x-[0.5] p-0">
-      <div className="relative w-[120px] min-h-[70px]  max-h-[70px] rounded-lg overflow-hidden flex-shrink-0">
+    <div className="bg-zinc-800 h-[75px] transition-all duration-300 ease-out hover:scale-[0.97] rounded-xl shadow-lg flex items-center space-x-[0.5] p-0">
+      <div className="relative w-[120px] min-h-[75px]  max-h-[75px] rounded-lg overflow-hidden flex-shrink-0">
         <Image
             src={image.includes('https://') ? image : imgbup}
             alt={title}
@@ -16,7 +16,7 @@ const EpisodeCard = ({ title, description, image, number, imgbup }) => {
 
       <div className="flex flex-col overflow-hidden">
         <h3 className="text-sm font-semibold ml-1  text-white truncate">{`${number}. ${title}`}</h3>
-        <p className="text-sm text-gray-300 ml-1 line-clamp-2">{description.includes('$undefined') ? "" : description}</p>
+        <p className="text-sm text-gray-300 ml-1 line-clamp-3">{description.includes('$undefined') ? "" : description}</p>
       </div>
     </div>
   );
