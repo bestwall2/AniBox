a0cab77401766e62f2980aa2d0b35db7d335fbec
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2025-06-04 18:32:36 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       // Add disallow rules here if there are specific paths that should not be crawled
       // e.g., disallow: '/admin/',
     },
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/sitemap.xml`,
+    sitemap: 'https://ani-play-sigma.vercel.app/sitemap.xml`,
   };
 }
