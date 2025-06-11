6cc195297d5e8407aebecdf82a3edf29be3cc3a2
bestwall2 <ahmed.dikha26@gmail.com>
2025-06-11 03:58:25 +0100

    update episodes card

--- components/CardsComp/EpisodesCard.tsx
+++ components/CardsComp/EpisodesCard.tsx
@@ -3,7 +3,7 @@ import { FaPlay } from "react-icons/fa";
 
 const EpisodeCard = ({ title, description, image, number, imgbup }) => {
   return (
-    <div className="bg-zinc-800 h-[75px] transition-all duration-300 ease-out hover:scale-[0.97] rounded-xl shadow-xl flex items-center space-x-[0.5]">
+    <div className="bg-zinc-800 h-[75px] transition-all pt-2 pb-2  duration-300 ease-out hover:scale-[0.97] rounded-xl shadow-xl flex items-center space-x-[0.3]">
         <div className="relative transition-all duration-300 ease-out hover:scale-[0.97] w-[120px] min-h-[75px] max-h-[75px] rounded-lg overflow-hidden flex-shrink-0">
             <Image
             src={image.includes('https://') ? image : imgbup}
@@ -16,11 +16,17 @@ const EpisodeCard = ({ title, description, image, number, imgbup }) => {
             <div className="absolute inset-0 flex items-center justify-center">
                 <FaPlay size={18} className="text-white opacity-90" />
             </div>
+            
+            {/* Episode number with dark, semi-transparent background */}
+            <div className="absolute font-semibold bottom-0 left-0 bg-black bg-opacity-60 text-white text-xs px-2 py-1 rounded-br-lg">
+                Ep {number}
+            </div>
+            
         </div>
         
         <div className="flex flex-col p-2 mt-3 mb-3 overflow-hidden">
             <h3 className="text-sm font-semibold text-white truncate">{`${number}. ${title}`}</h3>
-            <p className="text-sm text-gray-300 line-clamp-3">
+            <p className="text-sm text-gray-400 line-clamp-3">
             {description.includes('$undefined') ? "" : description}
             </p>
         </div>
