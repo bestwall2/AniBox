dda2c94a5d81c443ee88af4c943cedce736a3c73
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2025-11-22 17:03:57 +0000

    yuhyyhyh

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -122,7 +122,7 @@ const PlayerPageContent = () => {
             <img
               src={animeDetails.coverImage.extraLarge}
               alt="Cover"
-              className="h-[19vh] w-[14vh] rounded-xl object-cover shadow-lg"
+              className="min-h-[23vh] min-w-[14vh] max-h-[23vh] max-w-[14vh] rounded-xl object-cover"
             />
           ) : (
             <Skeleton className="h-[160px] w-[110px] rounded-xl" />
