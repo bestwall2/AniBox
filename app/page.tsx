feb81961181367b531584f9eb9de11fcc7114a9d
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2025-12-02 17:27:14 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     title: "AniBox - Watch Free Anime Online",
     description: "Discover and watch thousands of anime series and movies for free on AniBox. Your go-to source for the latest and greatest anime content.",
     // Optionally, add a specific Open Graph image for the homepage here
-    // images: [{ url: '/path/to/homepage-image.jpg' }],
+    images: [{ url: './images/logo.png' }],
   },
   // You can also add Twitter specific metadata if desired
   // twitter: {
