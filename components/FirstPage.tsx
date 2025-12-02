1bef47107b6344fbe5eba96c926d262010b2e44e
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2025-12-02 14:53:08 +0100

    Update FirstPage.tsx

--- components/FirstPage.tsx
+++ components/FirstPage.tsx
@@ -15,8 +15,14 @@ const FirstPage = () => {
         <div className="flex-1 overflow-auto">
           <Slider />
           <div className="Items container mx-auto p-4 pt-0 pb-5 flex flex-col">
-            <ListItems geners="Trending Anime 🔥" apiPath="/api/trending-anime" />
-            <ListItems geners="Top 100 Anime 🔝" apiPath="/api/top-anime" />
+            <ListItems 
+              geners="Trending Anime 🔥" 
+              apiPath="/api/trending-anime" 
+              param="font-semibold"/>
+            <ListItems 
+              geners="Top 100 Anime 🔝" 
+              apiPath="/api/top-anime" 
+              param="font-semibold"/>
             <ListItems
               geners="Favorites Anime ❤️"
               apiPath="/api/favorites-anime"
